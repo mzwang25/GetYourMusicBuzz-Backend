e0b8f7260e87c1e46c5eab38af0075d9a02efa90
michael wang <mzwang25@gmail.com>
2021-05-18 09:32:15 -0700

    test

--- main.py
+++ main.py
@@ -54,5 +54,4 @@ def receive():
     return "GOT!"
 
 if __name__ == '__main__':
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port=port, debug=True)
+  app.run(threaded=True, port=5000)
